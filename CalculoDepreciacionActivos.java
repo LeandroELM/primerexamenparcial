3667018d7735cf7f53e935dc6c4568eadc8672f8
Leandro Lacayo <leandrolacayo17@gmail.com>
2022-10-07 17:43:55 -0600

    Cuarto guardado

--- CalculoDepreciacionActivos.java
+++ CalculoDepreciacionActivos.java
@@ -7,21 +7,37 @@ public class CalculoDepreciacionActivos {
 	static double vidautil;
 	static Scanner lector=new Scanner(System.in);
 	public static void main(String[] args) {
-		double DepreAnual;
-		double costdelact,valordedesecho,vidautil;
 		
-		System.out.println("---------Bienvenido--------");
-		System.out.println("Ingrese que metodo desea usar.");
+		
+		System.out.println("---------------Bienvenido-------------");
+		System.out.println();
+		System.out.println("Ingrese que metodo desea usar:");
+		System.out.println();
 		System.out.println("1. Metodo de linea recta.");
 		System.out.println("2. Metodo de la suma de los digitos por año.");
 		System.out.println("3. metodo de las unidades producidas.");
-		System.out.println("4.Metodo de reduccion de saldos.");
+		System.out.println("4. Metodo de reduccion de saldos.");
+		System.out.println();
+		System.out.println("--------------------------------------");
 		int op = lector.nextInt();
 		
 		switch (op) {
 		
 		case 1: {
 			System.out.println("Que desea depresiar?");
+			double cosa = lector.nextDouble();
+			double costact,valordesecho,vidautil;
+			System.out.println("Ingrese el costo del activo");
+			costact=lector.nextDouble();
+			System.out.println("Ingrese el valor de desecho.");
+			valordesecho=lector.nextDouble();
+			System.out.println("Ingrese la vida util.");
+			vidautil=lector.nextDouble();
+			
+			cosa= (costact-valordesecho)/vidautil;
+			System.out.println("Su depresiacion anual es:" + cosa);
+		
+			
 			break;
 			
 			
