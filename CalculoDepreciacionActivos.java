145b9fafcb41600c5734cc039c860ad589508564
Leandro Lacayo <leandrolacayo17@gmail.com>
2022-10-07 17:06:01 -0600

    Primer Guardado

---
+++ CalculoDepreciacionActivos.java
@@ -0,0 +1,41 @@
+package Examen;
+
+import java.util.Scanner;
+
+public class CalculoDepreciacionActivos {
+	static Scanner lector=new Scanner(System.in);
+	public static void main(String[] args) {
+		double DepreAnual;
+		double costdelact,valordedesecho,vidautil;
+		
+		System.out.println("---------Bienvenido--------");
+		System.out.println("Ingrese que metodo desea usar.");
+		System.out.println("1. Metodo de linea recta.");
+		System.out.println("2. Metodo de la suma de los digitos por año.");
+		System.out.println("3. metodo de las unidades producidas.");
+		System.out.println("4.Metodo de reduccion de saldos.");
+		int op = lector.nextInt();
+		
+		switch(op) {
+		
+		
+		case:1 
+				
+				
+				
+				
+
+		
+		
+		
+		}
+		
+		
+
+	}
+		
+		
+		
+	
+
+}
